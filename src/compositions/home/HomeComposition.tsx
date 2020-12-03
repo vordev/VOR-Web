5d64865ba88b253b08b71543bb8962e46d530278
Horacio Garzon <horaciorafaelg30@gmail.com>
2020-12-03 12:49:07 -0300

    homepage update

--- src/compositions/home/HomeComposition.tsx
+++ src/compositions/home/HomeComposition.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Dispatch } from 'redux';
 import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Button, Card, CardContent } from '@material-ui/core';
+import { ArrowForward, Casino } from '@material-ui/icons';
 
 import { Container, ConnectWalletButton, Footer, Hero, Header } from 'components';
 import { RootState } from 'types';
@@ -24,16 +25,51 @@ export const HomeComposition = ({ account, history }: Props) => {
       <Header />
       <div className='body mt-100 mb-50'>
         <Container>
-          <div className='text-title text-black mb-30'>
-            We are ready to help you
+          <div>
+            <div className='text-title text-black mb-20'>
+              Start building with Developer docs.
+            </div>
+            <div className='text-small text-gray text-center'>
+              VOR supports On-Chain verifiable randomness generation.<br/>
+              You can start building the smart contracts with our Developer docs.
+            </div>
+            <div className='center-h mt-30'>
+              <Card className='card'>
+                <CardContent>
+                  <div className='center-v text-small text-black mb-30'><Casino /> &nbsp;&nbsp;Verifiable Randomness</div>
+                  <div className='flex-space mb-10'>
+                    <div className='text-tiny text-gray'>Overview</div>
+                    <ArrowForward className='text-tiny text-gray' />
+                  </div>
+                  <div className='flex-space mb-10'>
+                    <div className='text-tiny text-gray'>Get a Random Number</div>
+                    <ArrowForward className='text-tiny text-gray' />
+                  </div>
+                  <div className='flex-space mb-10'>
+                    <div className='text-tiny text-gray'>Verify Randomness Proof</div>
+                    <ArrowForward className='text-tiny text-gray' />
+                  </div>
+                  <div className='flex-space mb-10'>
+                    <div className='text-tiny text-gray'>Testnet Contracts</div>
+                    <ArrowForward className='text-tiny text-gray' />
+                  </div>
+                  <br/>
+                </CardContent>
+              </Card>
+            </div>
           </div>
-          <div className='text-small text-gray text-center'>
-            We have a global community and being always ready to help you.
-          </div>
-          <div className='center-h mt-30 mb-5'>
-            <Button className='btn-round'>
-              Get Support
-            </Button>
+          <div className='mb-50'>
+            <div className='text-title text-black mt-100 mb-30'>
+              We are ready to help you
+            </div>
+            <div className='text-small text-gray text-center'>
+              We have a global community and being always ready to help you.
+            </div>
+            <div className='center-h mt-30 mb-5'>
+              <Button className='btn-round'>
+                Get Support
+              </Button>
+            </div>
           </div>
         </Container>
       </div>
